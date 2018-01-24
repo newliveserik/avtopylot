5fee7570dfa0d091fbbc1cb484318ff56d5c7d2e
newliveserik@gmail.com <Dreamteam1985@>
2018-01-25 04:18:04 +0600

    кнопки регистраций сделал

---
+++ app/src/main/java/kz/avtopylot/trezvod/avtopylot/MainActivity.java
@@ -0,0 +1,25 @@
+package kz.avtopylot.trezvod.avtopylot;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                .setFontAttrId(R.attr.fontPath).build());
+        setContentView(R.layout.activity_main);
+    }
+}
